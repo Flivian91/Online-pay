b37e758a78f2447ba431622a9bf2756ba0faa348
Flivian91 <fliviandev@gmail.com>
2024-12-10 02:17:53 +0300

    commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,29 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 function HeroSection() {
   return (
-    <div
-      style={{
-        backgroundImage: "url('/banner.jpg')",
-        backgroundSize: "cover",
-      }}
-      className="h-[480px] w-full flex items-center justify-center"
-    >
-      <div className="flex flex-col gap-6 items-center text-center md:w-3/4">
-        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl uppercase text-center text-white italic">
-          Move your money around the world with M-Pesa and paypal
-        </h1>
-        <p className="text-base font-semibold text-stone-100 tracking-wide text-center">
-          Just link your M-PESA and PayPal accounts and enjoy a world of
-          possibilities
-        </p>
-        <Link
-          className="bg-[#0070ba] hover:bg-[#0070bae5] text-white border-2 rounded-full border-[#0070ba] text-lg px-12 md:w-1/3 py-3 font-semibold"
-          href={"/signup"}
-        >
-          Link Account
-        </Link>
+    <div className="w-full flex  justify-center bg-[#60cdff]">
+      <div className="md:space-y-12 space-y-24 pb-1 pt-16">
+        <div className="flex items-center justify-center">
+          <div className="flex items-center rounded-full border-2 border-black">
+            <button className="py-3 px-6 bg-black rounded-full text-white tracking-wide text-base font-semibold">
+              Personal
+            </button>
+            <button className="py-3 px-6 bg-transparent rounded-full text-black tracking-wide text-base font-semibold">
+              Business
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <h1 className="text-black lg:text-8xl  text-4xl w-[300px] transform scale-150 sm:scale-100 sm:text-6xl sm:w-[400px] text-center lg:w-[1000px] font-san tracking-tight font-semibold">
+            Pay, send, and save smarter
+          </h1>
+        </div>
+        <div className=" flex items-center justify-center px-3">
+          <div className="bg-white py-4 px-2 rounded-3xl w-full h-[400px] flex items-center flex-col">
+            <h1 className="text-4xl text-gray-700 font-bold py-4">PayPal</h1>
+            <Image
+              className="w-full h-full transform rounded-3xl"
+              src={"/hero.jpg"}
+              alt="Hero Image"
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { IoIosArrowDown, IoMdMenu } from "react-icons/io";
 import BusinessDropDown from "./BusinessDropDown";
 import PersonalDropDown from "./PersonalDropDown";
+import { BiMenu } from "react-icons/bi";
 
 function NavBar() {
   const [isBusinessOpen, setIsBusinessOpen] = useState(false);
@@ -17,27 +18,29 @@ function NavBar() {
 
   return (
     <div
-      className="relative flex items-center justify-between bg-white px-4 lg:px-28 shadow"
+      className="relative flex items-center gap-2 justify-between bg-white px-0 lg:px-8 shadow py-2 lg:py-4"
       onMouseLeave={handleOutsideClick}
     >
-      {/* Menu area */}
+      {/* Menu area
       <button className="text-[#0070ba] border-2 rounded-full border-[#0070ba] text-base px-4 py-2  xl:hidden font-semibold flex items-center gap-3">
         <IoMdMenu />
         <span>Menu</span>
-      </button>
+      </button> */}
       {/* Logo */}
-      <div className="hidden xl:flex">
-        <Image
-          className="transform scale-150 -translate-y-1 "
-          src={"/logo.svg"}
-          alt="Paypal Logo"
-          width={100}
-          height={100}
-        />
+      <div className="">
+        <div className=" h-9 w-9 md:w-[54px] md:h-[54px]">
+          <Image
+            className=" object-contain w-full"
+            src={"/paypal.svg"}
+            alt="Paypal Logo"
+            width={100}
+            height={100}
+          />
+        </div>
       </div>
 
       {/* Links Section */}
-      <div className="items-center gap-4 hidden xl:flex ">
+      <div className="flex-1 items-center justify-start gap-4 hidden lg:flex ">
         {/* Personal Dropdown */}
         <div
           onMouseEnter={() => setIsPersonalOpen(true)}
@@ -47,12 +50,12 @@ function NavBar() {
           <button
             onClick={() => setIsPersonalOpen(!isPersonalOpen)}
             className={
-              "flex hover:border-b-4 group border-b-4 border-transparent hover:border-[#0070ba] items-center gap-2 py-6 px-3 text-lg font-semibold text-[#2c2e2f] uppercase"
+              "flex group items-center gap-2 py-2 px-3 text-base rounded-full border hover:bg-[#60cdff4f] hover:text-black font-semibold text-black"
             }
           >
-            <span className="text-gray-500">Personal</span>
+            <span>Personal</span>
             <IoIosArrowDown
-              className={`text-2xl transition-transform duration-500 ${
+              className={`text-base transition-transform duration-500 ${
                 isPersonalOpen ? "-rotate-180" : "rotate-0"
               }`}
             />
@@ -71,12 +74,35 @@ function NavBar() {
           <button
             onClick={() => setIsBusinessOpen(!isBusinessOpen)}
             className={
-              "flex hover:border-b-4 group border-b-4 border-transparent hover:border-[#0070ba] items-center gap-2 py-6 px-3 text-lg font-semibold text-[#2c2e2f] uppercase"
+              "flex group items-center gap-2 py-2 px-3 text-base rounded-full hover:bg-[#60cdff4f] hover:text-black font-semibold text-black"
             }
           >
-            <span className="text-gray-500">Business</span>
+            <span>Small Business</span>
             <IoIosArrowDown
-              className={`text-2xl transition-transform duration-500 ${
+              className={`text-base transition-transform duration-500 ${
+                isBusinessOpen ? "-rotate-180" : "rotate-0"
+              }`}
+            />
+          </button>
+          {isBusinessOpen && (
+            <BusinessDropDown onClose={() => setIsBusinessOpen(false)} />
+          )}
+        </div>
+        {/* Business Dropdown */}
+        <div
+          onMouseEnter={() => setIsBusinessOpen(true)}
+          onMouseLeave={() => setIsBusinessOpen(false)}
+          className="relative"
+        >
+          <button
+            onClick={() => setIsBusinessOpen(!isBusinessOpen)}
+            className={
+              "flex group items-center gap-2 py-2 px-3 text-base rounded-full hover:bg-[#60cdff4f] hover:text-black font-semibold text-black"
+            }
+          >
+            <span>Enterprise</span>
+            <IoIosArrowDown
+              className={`text-base transition-transform duration-500 ${
                 isBusinessOpen ? "-rotate-180" : "rotate-0"
               }`}
             />
@@ -89,38 +115,36 @@ function NavBar() {
         {/* Static Link */}
         <button
           className={
-            "flex hover:border-b-4 group border-b-4 border-transparent text-gray-500 hover:border-[#0070ba] items-center gap-2 py-6 px-3 text-lg font-semibold  uppercase"
+            "flex group items-center gap-2 py-2 px-3 text-base rounded-full border border-transparent hover:border-gray-400/40 hover:text-black font-semibold text-black"
           }
         >
-          Partners and Developers
+          Developer
         </button>
-      </div>
-
-      {/* Logo At the center */}
-      <div className="xl:hidden">
-        <Image
-          className="xl:hidden"
-          src={"/single-logo.png"}
-          alt="Paypal Logo"
-          width={100}
-          height={100}
-        />
       </div>
 
       {/* Auth Links */}
       <div className="flex items-center gap-3">
         <Link
-          className="text-[#0070ba] border-2 rounded-full border-[#0070ba] text-base px-6 py-2 font-semibold underline"
+          className="text-black rounded-full hidden lg:block border border-transparent text-xs sm:text-base px-4 hover:border-gray-400/30 py-2 font-semibold"
+          href={"https://www.paypal.com/us/cshelp/personal"}
+        >
+          Help
+        </Link>
+        <Link
+          className="text-black border-2 md:border-4 rounded-full border-black text-xs sm:text-base transform hover:scale-105 px-6 py-1 md:py-2 font-semibold"
           href={"/signin"}
         >
           Log In
         </Link>
         <Link
-          className="bg-[#0070ba] text-white hidden xl:flex border-2 rounded-full border-[#0070ba] text-base px-4 py-2 font-semibold"
+          className="bg-black text-white flex  rounded-full border-2 md:border-4 border-transparent hover:text-black hover:border-black hover:bg-[#0070e0ab] text-xs sm:text-base px-4 py-1 md:py-2 font-semibold"
           href={"/signin"}
         >
           Sign Up
         </Link>
+        <button className="p-2 rounded-full border border-transparent hover:border-gray-300">
+          <BiMenu className="text-2xl rounded" />
+        </button>
       </div>
     </div>
   );
